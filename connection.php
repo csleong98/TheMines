baab659a36601e04ea0a4a7a7040451a6c0ff447
Chee Seng Leong <macintoshleong@gmail.com>
2017-12-25 12:49:26 +0800

    new updates

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = mysqli_connect("103.6.196.51","boleh","TWSB2018", "boleh_mines2017", "", "3306");
+	$conn = mysqli_connect("localhost","boleh","TWSB2018");
 	if(mysqli_connect_errno())
 	{
 		die("<script>alert('Error in connection!');window.history.go(-1);</script>");
--- submit.php
+++ submit.php
@@ -1,6 +1,6 @@
 <?php 
 
-    include('connection.php');
+    include 'connection.php';
 
     $teamname = mysqli_real_escape_string($conn, $_POST['teamName']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
@@ -13,13 +13,13 @@
 
 
     $sql = "INSERT INTO rooms(teamName, email, player1, pass1, phone1, player2, pass2, phone2) ".
-    "values ('$teamname','$email','$player1','$passnum1','$phonenum1','$player2','$passnum2','$phonenum2');";
+    "values('$teamname','$email','$player1','$passnum1','$phonenum1','$player2','$passnum2','$phonenum2');";
 
     mysqli_query($conn, $sql);
 
     if (mysqli_affected_rows($conn)<=0) {
         echo "<script>alert('Unable to register! \\nPlease try again later.');";
-        die("window.history.go(-1);</script>");
+        die("window.history.go(0);</script>");
         
     }
 
