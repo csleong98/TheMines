9a112da00f7fbf192dee25993cd608c1cbd0a72a
Chee Seng Leong <macintoshleong@gmail.com>
2017-12-26 17:39:38 +0800

    added more validation functions

--- submit.php
+++ submit.php
@@ -14,47 +14,50 @@
     $sqlcheck = "SELECT * FROM team";
     $result = mysqli_query($conn, $sqlcheck);
 
-    // if(mysqli_num_rows($result)<=0) {
-    //     echo "no results";
-    // }
-    // else 
-    // {
-        if($rows = mysqli_fetch_array($result)) {
-            if($rows['teamName'] === $teamname) {
-                echo "<script>alert('Team name already registered');";
-                die("window.history.go(-1);</script>");  
-            }
-                elseif($rows['email'] === $email) {
-                    echo "<script>alert('Email already registered');";
-                    die("window.history.go(-1);</script>"); 
-                }
-                    elseif($rows['player1'] === $player1) {
-                        echo "<script>alert('Player name already registered');";
-                        die("window.history.go(-1);</script>"); 
+    if(mysqli_num_rows($result)>=100) {
+        echo "<script>alert('Registration closed');";
+        echo "window.location.href='./index.html';</script>";
+    }
+    else 
+    {
+        $rows = $result->num_rows;
+
+                while($rows = mysqli_fetch_array($result)) {
+                    if($rows['teamName'] === $teamname) {
+                        echo "<script>alert('Team name already registered');";
+                        die("window.history.go(-1);</script>");  
                     }
-                        elseif($rows['pass1'] === $passnum1) {
-                            echo "<script>alert('IC /Passport number already registered');";
+                        elseif($rows['email'] === $email) {
+                            echo "<script>alert('Email already registered');";
                             die("window.history.go(-1);</script>"); 
                         }
-                            elseif($rows['phone1'] === $phonenum1) {
-                                echo "<script>alert('Phone number already registered');";
+                            elseif($rows['player1'] === $player1) {
+                                echo "<script>alert('This player has already registered');";
                                 die("window.history.go(-1);</script>"); 
                             }
-                                elseif($rows['player2'] === $player2) {
-                                    echo "<script>alert('Player name already registered');";
+                                elseif($rows['pass1'] === $passnum1) {
+                                    echo "<script>alert('Player 1 IC /Passport number already registered');";
                                     die("window.history.go(-1);</script>"); 
                                 }
-                                    elseif($rows['pass2'] === $passnum2) {
-                                        echo "<script>alert('IC /Passport number already registered');";
+                                    elseif($rows['phone1'] === $phonenum1) {
+                                        echo "<script>alert('Player 1 phone number already registered');";
                                         die("window.history.go(-1);</script>"); 
                                     }
-                                        elseif($rows['phone2'] === $phonenum2) {
-                                            echo "<script>alert('Phone number already registered');";
+                                        elseif($rows['player2'] === $player2) {
+                                            echo "<script>alert('This player has already registered');";
                                             die("window.history.go(-1);</script>"); 
                                         }
-        }
+                                            elseif($rows['pass2'] === $passnum2) {
+                                                echo "<script>alert('Player 2 IC /Passport number already registered');";
+                                                die("window.history.go(-1);</script>"); 
+                                            }
+                                                elseif($rows['phone2'] === $phonenum2) {
+                                                    echo "<script>alert('Player 1 phone number already registered');";
+                                                    die("window.history.go(-1);</script>"); 
+                                                }
+                }
         
-    // }
+    }
 
     if ($teamname == '') {
         echo "<script>alert('Team name is not inserted!');";
@@ -66,50 +69,57 @@
             die("window.history.go(-1);</script>");   
           }
 
-            elseif (!is_numeric($passnum1)) {
-                echo "<script>alert('Player 1 passport is not a number');";
-                die("window.history.go(-1);</script>");  
+            elseif ($player1 == $player2) {
+                echo "<script>alert('Player 1 name cannot be the same as player 2');";
+                die("window.history.go(-1);</script>"); 
             }
 
-                elseif (!is_numeric($phonenum1)) {
-                    echo "<script>alert('Player 1 phone number is not a number');";
+                elseif (!is_numeric($passnum1)) {
+                    echo "<script>alert('Player 1 passport is not a number');";
                     die("window.history.go(-1);</script>");  
                 }
 
-                elseif ($player2 == '') {
-                    echo "<script>alert('Player 2 name is not inserted!');";
-                    die("window.history.go(-1);</script>");   
-                  }
-        
-                    elseif (!is_numeric($passnum2)) {
-                        echo "<script>alert('Player 2 passport is not a number');";
+                    elseif (!is_numeric($phonenum1)) {
+                        echo "<script>alert('Player 1 phone number is not a number');";
                         die("window.history.go(-1);</script>");  
                     }
-        
-                        elseif (!is_numeric($phonenum2)) {
-                            echo "<script>alert('Player 2 phone number is not a number');";
+
+                    elseif ($player2 == '') {
+                        echo "<script>alert('Player 2 name is not inserted!');";
+                        die("window.history.go(-1);</script>");   
+                    }
+
+                        elseif (!is_numeric($passnum2)) {
+                            echo "<script>alert('Player 2 passport is not a number');";
                             die("window.history.go(-1);</script>");  
                         }
+            
+                            elseif (!is_numeric($phonenum2)) {
+                                echo "<script>alert('Player 2 phone number is not a number');";
+                                die("window.history.go(-1);</script>");  
+                            }
 
-          if(isset($_POST['check']) && $_POST['check']="1")
-          {
-                $sql = "INSERT INTO team(teamName, email, player1, pass1, phone1, player2, pass2, phone2) ".
-                "values('$teamname','$email','$player1','$passnum1','$phonenum1','$player2','$passnum2','$phonenum2');";
-            
-                mysqli_query($conn, $sql);
-            
-                if (mysqli_affected_rows($conn)<=0) {
-                    echo "<script>alert('Unable to register! \\nPlease fill in all fields.');";
-                    die("window.history.go(-1);</script>");
-                    
-                }
-            
-                echo "<script>alert('Register Successfully! Please proceed to the game area.');";
-                echo "window.location.href='./tutorial.html';</script>";
-            }
+                                if(isset($_POST['check']) && $_POST['check']="1")
+                                {
+                                        $sql = "INSERT INTO team(teamName, email, player1, pass1, phone1, player2, pass2, phone2) ".
+                                        "values('$teamname','$email','$player1','$passnum1','$phonenum1','$player2','$passnum2','$phonenum2');";
+                                    
+                                        mysqli_query($conn, $sql);
+                                    
+                                        if (mysqli_affected_rows($conn)<=0) {
+                                            echo "<script>alert('Unable to register! \\nPlease fill in all fields.');";
+                                            die("window.history.go(-1);</script>");
+                                            
+                                        }
+                                    
+                                        echo "<script>alert('Register Successfully! Please proceed to the game area.');";
+                                        echo "window.location.href='./tutorial.html';</script>";
+                                    }
 
-          else {
-            echo "<script>alert('Terms & Conditions is not checked');";
-            die("window.history.go(-1);</script>");  
-          }
+                                else {
+                                    echo "<script>alert('Terms & Conditions is not checked');";
+                                    die("window.history.go(-1);</script>");  
+                                }
+
+          mysqli_close($conn);
 ?>
